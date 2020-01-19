cac83e4e2002e04f27f1d3864030617fa5b811a6
Brangi <brangi@Brangis-MBP.attlocal.net>
2020-01-19 17:58:34 -0600

    More progress:
    Fixes
    Resolve calls
    Parse XML and JSON
    Add endpoint call
    
    TODO: Tests and improvements

--- api_controller.js
+++ api_controller.js
@@ -1,69 +1,106 @@
 const axios = require('axios');
 const _ = require('lodash');
+const util = require('util');
 const conf = require('./conf');
+const parserXML = require('xml2js');
+const parser = new parserXML.Parser({explicitArray : false});
+const parseXml = util.promisify(parser.parseString);
 
 const fixed = str => str.replace(/\s/g, '').toLowerCase();
 const random = arr => _.shuffle(arr)[0];
 const makeParamsReq1 = (c,lat, lng, rad) =>
-  `${c.response}?location=${lat},${lng}&radius=${rad || conf.minRadius}&type=${c.type}&key=${c.api_key}&language=${c.language}`;
+  `${c.response}?location=${lat},${lng}&radius=${rad}&type=${c.type}&key=${c.api_key}&language=${c.language}`;
 const makeParamsReq2 = (c,tok) =>
   `${c.response}?pagetoken=${tok}&key=${c.api_key}`;
 
-//const makeRequest = ({customer, lat, lng, type, token}) => {//
-//
+const makeRequest =  async (customer, lat, lng, token, radius) => {
+  let response, requestParams;
+  if(token){
+    switch(customer.response) {
+    case 'xml':
+      requestParams = makeParamsReq2(customer, token);
+      response = await axios.get(`${conf.placeAPI}${requestParams}`);
+      response = await parseXml(response.data);
+      response = {
+        status: response.PlaceSearchResponse.status,
+        results: response.PlaceSearchResponse.result || [],
+        next_page_token: response.PlaceSearchResponse.next_page_token && response.PlaceSearchResponse.next_page_token || undefined
+      };
+      break;
+    case 'json':
+      requestParams = makeParamsReq2(customer, token);
+      response = await axios.get(`${conf.placeAPI}${requestParams}`);
+      response = response.data;
+      break;
+    }
+  } else {
+    switch(customer.response) {
+    case 'xml':
+      requestParams = makeParamsReq1(customer, lat, lng, radius);
+      response = await axios.get(`${conf.placeAPI}${requestParams}`);
+      response = await parseXml(response.data);
+      response = {
+        status: response.PlaceSearchResponse.status,
+        results: response.PlaceSearchResponse.result || [],
+        next_page_token: response.PlaceSearchResponse.next_page_token && response.PlaceSearchResponse.next_page_token || undefined
+      };
+      break;
+    case 'json':
+      requestParams = makeParamsReq1(customer, lat, lng, radius);
+      response = await axios.get(`${conf.placeAPI}${requestParams}`);
+      response = response.data;
+      break;
+    }
+  }
 
-//};
+  return response;
+};
 
 const findNearBy = async (lat, lng, customerName)=> {
   const customer = conf.customerInfo.find(c => fixed(c.name) === fixed(customerName));
   if(customer){
-    const reqParams1 = makeParamsReq1(customer, lat, lng);
-    const urlReq = `${conf.placeAPI}${reqParams1}`;
-    const res1 = await axios.get(urlReq);
-    let tokenNext = res1.data.next_page_token;
     let currentRadius = conf.minRadius;
+    let radiusTry = 0;
+    const response = await makeRequest(customer, lat, lng, undefined, currentRadius);
+    let tokenNext = response.next_page_token;
     let placesResult;
     let placesReturned = 0;
 
     while(!placesResult || placesReturned < customer.request_no){
-      console.log(placesReturned);
-      console.log(currentRadius);
       if(placesResult && placesResult.length > 0 && tokenNext){
-        const reqParams2 = makeParamsReq2(customer, tokenNext);
-        const res2 = await axios.get(`${conf.placeAPI}${reqParams2}`);
-
-        tokenNext = res2.data && res2.data.next_page_token || undefined;
-        placesResult = [...placesResult, ...res2.data.results];
+        const response = await makeRequest(customer, lat, lng, tokenNext);
+        tokenNext = response.next_page_token;
+        placesResult = [...placesResult, ...response.results];
         placesResult  = _.uniqBy(placesResult, 'id');
+        placesResult = placesResult.slice(0,customer.request_no);
         placesReturned = placesResult.length;
+        radiusTry++;
       } else {
-
-        if(!tokenNext && placesResult.length > 0 ){
-          let reqParams1;
-          currentRadius = currentRadius + 20000;
-          if(currentRadius >= conf.maxRadius){
+        if(!tokenNext && placesResult && placesResult.length > 0 ){
+          currentRadius = currentRadius + 10000;
+          let response;
+          if(currentRadius >= conf.maxRadius || radiusTry > conf.maxRadiusTry){
             const randomPlace = random(placesResult);
             currentRadius = conf.minRadius;
-            reqParams1 = makeParamsReq1(customer, randomPlace.geometry.location.lat , randomPlace.geometry.location.lng, conf.minRadius);
+            if(radiusTry > conf.maxRadiusTry) radiusTry = 0;
+            response = await makeRequest(customer, randomPlace.geometry.location.lat, randomPlace.geometry.location.lng, undefined, currentRadius);
           } else {
-            reqParams1 = makeParamsReq1(customer, lat, lng, currentRadius);
+            response = await makeRequest(customer, lat, lng, undefined, currentRadius);
           }
-          const urlReq = `${conf.placeAPI}${reqParams1}`;
-          const res = await axios.get(urlReq);
-          tokenNext = res.data && res.data.next_page_token || undefined;
-
-          placesResult = [...placesResult, ...res.data.results];
+          tokenNext = response.next_page_token;
+          placesResult = [...placesResult, ...response.results];
           placesResult  = _.uniqBy(placesResult, 'id');
           placesResult = placesResult.slice(0,customer.request_no);
           placesReturned = placesResult.length;
+          radiusTry++;
         } else {
-          placesResult = [...res1.data.results];
+          placesResult = [...response.results];
           placesResult = placesResult.slice(0,customer.request_no);
           placesReturned = placesResult.length;
         }
       }
-
     }
+    console.log(placesResult.length)
     return placesResult;
   } else {
     console.log('Not found');
@@ -71,5 +108,13 @@ const findNearBy = async (lat, lng, customerName)=> {
 
 };
 
-findNearBy(19.432777, -99.133217, 'Sunrise Bank');
+
+module.exports = {
+  findNearBy
+};
+
+//findNearBy(34.0522342,  -118.2436849, 'Happy Credit Union'); //Los angeles Example
+//findNearBy(34.0522342,  -118.2436849, 'Paris FCU'); //Los angeles Example
+//findNearBy(34.0522342,  -118.2436849, 'Sunrise Bank'); //Los angeles Example
+
 
--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
+const ctrl = require('./api_controller');
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/places', (req, res) => {
-  res.send({});
+app.get('/places', async(req, res) => {
+  const {lat, lng, name} = req.query;
+  const result = await ctrl.findNearBy(Number(lat),  Number(lng), name);
+  res.send(result);
 });
 
 app.listen(3000, () => {
